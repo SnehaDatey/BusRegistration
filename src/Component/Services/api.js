150bbb581cad8ba1fca6f3dc0438c4997c185284
SnehaDatey <snehadatey@gmail.com>
2024-11-05 10:46:11 +0530

    env file created and api file updated

--- src/Component/Services/api.js
+++ src/Component/Services/api.js
@@ -1,7 +1,9 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://3.109.124.47:8000/'; 
+//const API_BASE_URL = 'http://3.109.124.47:8000/'; 
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
